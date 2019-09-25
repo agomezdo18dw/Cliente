24e33c99220ba724a1fed7100669a8369aec0e64
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-26 00:24:05 +0200

    Echo ejercicio 4

--- Actividad 2/Ejercicio4/js.js
+++ Actividad 2/Ejercicio4/js.js
@@ -3,11 +3,18 @@
  *Nota: tendréis que investigar como es la conversión y únicamente se podrá emplear una función para las dos conversiones.
  *El ejercicio deberá de ejecutar tres veces la función...
  */
-let temperatura = () =>{ 
-    
+let temperatura = (temp, tipoTemp) =>{ 
+    if (tipoTemp == "c"){
+        let tempFinal = (temp * 9 / 5) + 32
+        document.write("La temperatura " + temp + "ºC son: " + tempFinal + "ºF<br>");
+    }
+    else {
+        let tempFinal = (temp - 32) * 5 / 9;
+        document.write("La temperatura " + temp + "ºF son: " + tempFinal + "ºC<br>");
+    }
 }
 for (let i = 0; i < 3; i++){
     let temp = parseInt(prompt("¿Que temperatura hace?"));
-    let tipoTemp = parseInt(prompt("¿En que tipo de temperatura quieres saberlo?"));
-    
+    let tipoTemp = prompt("¿En que tipo de temperatura lo has dicho?(C o F)");
+    temperatura(temp, tipoTemp);
 }
