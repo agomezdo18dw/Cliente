a5208d29023bd8ece1e75f4178ff76f46d91a367
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-26 00:36:28 +0200

    Borrada una cosa

--- Actividad 2/Ejercicio5/js.js
+++ Actividad 2/Ejercicio5/js.js
@@ -1,17 +1,2 @@
 /*
  */
-let temperatura = (temp, tipoTemp) =>{ 
-    if (tipoTemp == "c"){
-        let tempFinal = (temp * 9 / 5) + 32
-        document.write("La temperatura " + temp + "ºC son: " + tempFinal + "ºF<br>");
-    }
-    else {
-        let tempFinal = (temp - 32) * 5 / 9;
-        document.write("La temperatura " + temp + "ºF son: " + tempFinal + "ºC<br>");
-    }
-}
-for (let i = 0; i < 3; i++){
-    let temp = parseInt(prompt("¿Que temperatura hace?"));
-    let tipoTemp = prompt("¿En que tipo de temperatura lo has dicho?(C o F)");
-    temperatura(temp, tipoTemp);
-}
