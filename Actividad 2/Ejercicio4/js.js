6980be7db3393fe1654ae1faa0da69eea1430ee0
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-25 14:29:50 +0200

    Editando index ejercicio4

---
+++ Actividad 2/Ejercicio4/js.js
@@ -0,0 +1,13 @@
+/*Función que convierte los grados de  Celsius a Fahrenheit o viceversa. El usuario 
+ *introducirá la temperatura junto con el tipo de cambio que quiere realizar.
+ *Nota: tendréis que investigar como es la conversión y únicamente se podrá emplear una función para las dos conversiones.
+ *El ejercicio deberá de ejecutar tres veces la función...
+ */
+let temperatura = () =>{ 
+    
+}
+for (let i = 0; i < 3; i++){
+    let temp = parseInt(prompt("¿Que temperatura hace?"));
+    let tipoTemp = parseInt(prompt("¿En que tipo de temperatura quieres saberlo?"));
+    
+}
