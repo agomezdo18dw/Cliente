a04bef0dfe68cf340b7ff060fce7672846eeca4d
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-09-26 13:55:11 +0200

    Intento de poner comprobaciones pero tiene errores

--- Actividad 2/Ejercicio2/js.js
+++ Actividad 2/Ejercicio2/js.js
@@ -1,12 +1,51 @@
 /*Calculo de la edad de tu perro o gato.
  *Crea una función que calcule la edad de tu perro o tu gato.
  *La función recibirá la edad de tu mascota y realizará la *conversión:
- *1 año humano = 7 años de un perro</i>
+ *1 año humano = 7 años de un perro
  *El ejercicio deberá de ejecutar tres veces la función...
  */
+
+//Funcion que comprueba si hay numeros en el texto que le introduzcamos
+let tieneLetras = (texto) => {
+    //Creamos un string con los numeros
+    const letras = "abcdefghijklmnñopqrstuvwxyz";
+    //Pasamos el texto entero a minusculas
+    texto = texto.toLowerCase();
+    //Bucle que comprueba si en el texto hay numeros al leer letra con letra con el estring de antes
+    for(i=0; i < texto.length; i++){
+        if (letras.indexOf(texto.charAt(i),0) != -1){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Funcion que comprueba si hay simbolos en el texto que le introduzcamos
+let tieneSimbolos = (texto) => {
+    //Creamos un string con los simbolos
+    const simbolos = "!=?¿.,/&%$'-_;:<>[]{}";
+    //Bucle que comprueba si en el texto hay simbolos al leer letra con letra con el estring de antes
+    for(i=0; i < texto.length; i++){
+        if (simbolos.indexOf(texto.charAt(i),0) != -1){
+            return true;
+        }
+    }
+    return false;
+} 
+
 let calcularEdad = () => {
-    let animal = prompt("¿Que mascota tienes?");
+    /*let animal = prompt("¿Que mascota tienes?");
+    if (animal != "perro" || animal != "gato")
+        while(animal != "perro" || animal != "gato")
+            animal = prompt("Solo se admite perro o gato");*/
+    
     let edad = parseInt(prompt("¿Que edad tiene esa mascota?"));
+    //Condicion que hace comprobacion de si el apellido tiene solo letras
+    if(tieneLetras(edad) || tieneSimbolos(edad))
+        //Bucle que se repitehasta que solo haya letras en el apellido
+        while (tieneLetras(edad) || tieneSimbolos(edad))
+            edad = prompt("Eso no es un numero, ¿Que edad tiene esa mascota?");
+    
     if (animal == "perro"){
         let edadP = edad * 7;
         document.write("Tu perro tiene "+edad+" en años humanos, en años de perros tiene "+edadP+" años<br>");
