7b10469d03d1fc2ef2d7e97db41079e6436271f5
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-11-28 20:46:05 +0100

    Continuo  con el juego de los dados

---
+++ Objetos/EjercicioLibre/js/Dados/Dado.js
@@ -0,0 +1,14 @@
+class Dado {
+    constructor(lados) {
+        this.lados = lados;
+    };
+
+    setLados(lados) {
+        this.lados = lados;
+    }
+
+    getLados() {
+        return this.lados;
+    }
+};
+export { Dado };
