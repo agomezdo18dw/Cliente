d6c3111588bd3bd3e867190973d72d3a12e41816
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-10 14:10:53 +0100

    Pequeño cambio en el estilo del codigo

--- AJAX/Clase/Ejercicio1/js.js
+++ AJAX/Clase/Ejercicio1/js.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
         if (request.status >= 200 && request.status < 400) {
             for (let i = 0; i < data.length; i++) {
                 let fila = '<tr>';
-                fila += '<td>'+data[i].id+'</td>';
-                fila += '<td>'+data[i].first_name+'</td>';
-                fila += '<td>'+data[i].last_name+'</td>';
-                                fila += '<td>'+data[i].email+'</td>';
-                fila += '<td>'+data[i].gender+'</td>';
+                fila += '<td>' + data[i].id + '</td>';
+                fila += '<td>' + data[i].first_name + '</td>';
+                fila += '<td>' + data[i].last_name + '</td>';
+                fila += '<td>' + data[i].email + '</td>';
+                fila += '<td>' + data[i].gender + '</td>';
                 fila += '</tr>';
                 $('#tabla').append(fila);
             }
