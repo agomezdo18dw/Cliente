7b57178d5c6f1deb398ce005ea21b5a70861380c
agomezdo18dw <agomezdo18dw@ikzubirimanteo.com>
2019-12-12 12:12:25 +0100

    Continuo ejercicio de SWAPI

--- AJAX/Clase/StarWarsAPI/js.js
+++ AJAX/Clase/StarWarsAPI/js.js
@@ -1,20 +1,69 @@
 $(document).ready(function () {
-    
-    
-    $.get("https://swapi.co/", function (data, status) {
-        if (status == "success") {
-            for (let i = 0; i < data.length; i++) {
-                let fila = '<tr>';
-                fila += '<td>' + data[i].id + '</td>';
-                fila += '<td>' + data[i].first_name + '</td>';
-                fila += '<td>' + data[i].last_name + '</td>';
-                fila += '<td>' + data[i].email + '</td>';
-                fila += '<td>' + data[i].gender + '</td>';
-                fila += '</tr>';
-                $('#tabla').append(fila);
+
+    $('#getJquery').click(function () {
+        let numero = document.getElementById('numero').value;
+        let tipos = document.getElementById("tipos");
+        let tipo = tipos.options[tipos.selectedIndex].value;
+
+        $.get("https://swapi.co/api/" + tipo + "/" + numero + "/", function (data, status) {
+            if (status == "success") {
+                console.log(data)
+            } else {
+                console.log('error')
             }
+        });
+    });
+
+    $('#postJquery').click(function () {
+        let numero = document.getElementById('numero').value;
+        let datos = document.getElementById('datos').value;
+        $.post("https://my-json-server.typicode.com/typicode/demo/posts", {
+                id: numero,
+                datos: datos
+            },
+            function (data, status) {
+                if (status == "success") {
+                    console.log(data)
+                } else {
+                    console.log('error')
+                }
+            });
+    });
+});
+
+let getJS = () => {
+    let numero = document.getElementById('numero').value;
+    let tipos = document.getElementById("tipos");
+    let tipo = tipos.options[tipos.selectedIndex].value;
+    //Preparamos la llamada
+    let request = new XMLHttpRequest();
+    //Definimos el tipo de peticion asi com la url
+    request.open('GET', 'https://swapi.co/api/' + tipo + '/' + numero + '/', true);
+    request.onload = function () {
+        //almacenamos JSON
+        let data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data)
         } else {
             console.log('error')
         }
-    });
-});
+    }
+    request.send();
+}
+
+let postJs = () => {
+    let numero = document.getElementById('numero').value;
+    let datos = document.getElementById('datos').value;
+    //Preparamos la llamada
+    let request = new XMLHttpRequest();
+    
+    request.open('POST', 'https://my-json-server.typicode.com/typicode/demo/posts', true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200)
+            console.log(this.responseText);
+        else 
+            console.log("error")
+    };
+    request.send("id="+numero+"&datos="+datos);
+}
